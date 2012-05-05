5f3123fa8b467f300f997e93319cb3fa43ac83bc
Michael Babker <mbabker@flbab.com>
2012-05-05 07:14:58 +0200

    Optimizations for JHttpTransportCurl
    - Add the ability to set HTTP Authentication (some APIs still use it)
    - Modify processing to get response headers/body separated in case of HTTP redirects

--- libraries/joomla/http/transport/curl.php
+++ libraries/joomla/http/transport/curl.php
@@ -118,6 +118,17 @@ class JHttpTransportCurl implements JHttpTransport
 			$headers[CURLOPT_USERAGENT] = $userAgent;
 		}
 
+		// Check if we have authentication credentials; we're making a couple of assumptions about the user's credentials
+		// TODO: Only set the credentials for HTTP Authentication
+		if (strlen($this->options->get('api.username')) >= 2 && strlen($this->options->get('api.password')) >= 4)
+		{
+			$options[CURLOPT_HTTPAUTH] = CURLAUTH_ANY;
+			$options[CURLOPT_USERPWD] = $this->options->get('api.username') . ':' . $this->options->get('api.password');
+
+			// We need to set this so we can forward the authentication on redirects
+			$options[CURLOPT_UNRESTRICTED_AUTH] = true;
+		}
+
 		// Set the request URL.
 		$options[CURLOPT_URL] = (string) $uri;
 
@@ -126,6 +137,9 @@ class JHttpTransportCurl implements JHttpTransport
 
 		// Return it... echoing it would be tacky.
 		$options[CURLOPT_RETURNTRANSFER] = true;
+
+		// Follow redirects
+		$options[CURLOPT_FOLLOWLOCATION] = true;
 
 		// Set the cURL options.
 		curl_setopt_array($ch, $options);
@@ -153,17 +167,22 @@ class JHttpTransportCurl implements JHttpTransport
 		$return = new JHttpResponse;
 
 		// Split the response into headers and body.
-		$response = explode("\r\n\r\n", $content, 2);
+		$response = explode("\r\n\r\n", $content);
 
-		// Get the response headers as an array.
-		$headers = explode("\r\n", $response[0]);
+		// The last item in the array should be the body, get it
+		$return->body = array_pop($response);
 
-		// Set the body for the response.
-		$return->body = $response[1];
+		// Get the last set of response headers from the array.
+		$headers = array_pop($response);
+
+		// Explode the headers into an array
+		$headers = explode("\r\n", $headers);
 
 		// Get the response code from the first offset of the response headers.
 		preg_match('/[0-9]{3}/', array_shift($headers), $matches);
+
 		$code = $matches[0];
+
 		if (is_numeric($code))
 		{
 			$return->code = (int) $code;
